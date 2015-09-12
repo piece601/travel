a4089fc082261301d5472bb2693fe3ee179dcb10
Piece Chao <piece601@hotmail.com>
2015-09-12 12:09:13 +0800

    更新後台 ulnav

--- application/views/admin/product/_ulnav.php
+++ application/views/admin/product/_ulnav.php
@@ -1,4 +1,5 @@
 <ul class="nav nav-tabs">
   <li id="aluminium"><a href="<?php echo base_url('admin/product/index') ?>">鋁鎂合金</a></li>
   <li id="polycarbonate"><a href="<?php echo base_url('admin/product/index/polycarbonate') ?>">聚碳酸酯</a></li>
+  <li id="fitting"><a href="<?php echo base_url('admin/product/index/fit') ?>" class="">配件</a></li>
 </ul>
